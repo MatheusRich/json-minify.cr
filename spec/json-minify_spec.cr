c819c09202fda582cff56642ef8ba6f944e5ace8
Matheus Richard <matheusrichardt@gmail.com>
2020-05-25 21:25:23 -0300

    Add basic structure

---
+++ spec/json-minify_spec.cr
@@ -0,0 +1,9 @@
+require "./spec_helper"
+
+describe Json::Minify do
+  # TODO: Write tests
+
+  it "works" do
+    false.should eq(true)
+  end
+end
---
+++ src/json-minify.cr
@@ -0,0 +1,6 @@
+# TODO: Write documentation for `Json::Minify`
+module Json::Minify
+  VERSION = "0.1.0"
+
+  # TODO: Put your code here
+end
